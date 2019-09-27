fc5cb1a75351c56ffcc9ebe01037bfc767c8960f
Ken Gilmer <kgilmer@gmail.com>
2019-09-27 08:49:49 -0700

    Move to JSON model for package metadata.

--- build.sh
+++ build.sh
@@ -5,79 +5,66 @@
 set -e
 
 # Input Parameters
-PPA_URL=$1
-BUILD_DIR=$2
+PACKAGE_MODEL_FILE=$( realpath $1 )
+PPA_URL=$2
+BUILD_DIR=$3
+
+if [ "$PACKAGE_MODEL_FILE" == "" ]; then
+    echo "Usage: build.sh <package model> <user PPA> <build dir>"
+    echo "Must specify a package model file."
+    exit 1
+fi
 
 if [ "$PPA_URL" == "" ]; then
-    echo "Usage: build.sh <user PPA> <build dir>"
+    echo "Usage: build.sh <package model> <user PPA> <build dir>"
     echo "Must specify a PPA."
     exit 1
 fi
 
 if [ "$BUILD_DIR" == "" ]; then
-    echo "Usage: build.sh <user PPA> <build dir>"
+    echo "Usage: build.sh <package model> <user PPA> <build dir>"
     echo "Must specify a build dir."
     exit 1
 fi
 
-# Repositories
-# package name | upstream source URL | debian branch
-repos=( 
-    "regolith-i3-gaps-config||master" 
-    "regolith-styles||master"
-    "regolith-st|https://dl.suckless.org/st/st-0.8.2.tar.gz|debian"
-    "regolith-i3xrocks-config||master"
-    "regolith-gnome-flashback||master"
-    "regolith-desktop||debian"
-    "regolith-gdm3-theme||master"
-    "regolith-conky-config||master"
-    "regolith-xeventbind||master"
-    "regolith-assets||master"
-    "regolith-rofi-config||master"
-    "regolith-compton-config||master"
-    )
-base_url="https://github.com/regolith-linux/"
-
 # Checkout
 function checkout {
-    REPO="$base_url$repo.git"
+    echo "Checking out ${packageModel[gitRepoUrl]}"
 
-    echo "Checking out $REPO"
-
-    git clone $REPO -b $pkg_branch
+    git clone ${packageModel[gitRepoUrl]} -b ${packageModel[packageBranch]} $BUILD_DIR/${packageModel[packageName]}
 }
 
 # Package 
 function package {
-    cd $repo
+    cd $BUILD_DIR/${packageModel[packageName]}/${packageModel[buildPath]}
     debian_version=`dpkg-parsechangelog --show-field Version | cut -d'-' -f1`
-    cd ..
+    cd $BUILD_DIR
 
-    if [ "$src_url" != "" ]; then
-        echo "Downloading source from $src_url..."
-        wget $src_url -O $repo\_$debian_version.orig.tar.gz
+    if [ "${packageModel[upstreamTarball]}" != "" ]; then
+        echo "Downloading source from ${packageModel[upstreamTarball]}..."
+        wget ${packageModel[upstreamTarball]} -O ${packageModel[packageName]}\_$debian_version.orig.tar.gz
     else
         echo "Generating source tarball from git repo."
-        tar cfzv $repo\_$debian_version.orig.tar.gz --exclude .git\* --exclude debian $repo
+        tar cfzv ${packageModel[packageName]}\_$debian_version.orig.tar.gz --exclude .git\* --exclude debian ${packageModel[packageName]}
     fi
 }
 
 # Build
 function build {
-    echo "Building $REPO"
-    cd $repo
+    echo "Building ${packageModel[packageName]}"
+    cd $BUILD_DIR/${packageModel[packageName]}/${packageModel[buildPath]}
     debuild -S -sa
-    cd ..
+    cd $BUILD_DIR
 }
 
 # Publish
 function publish {
-    echo "Publishing source package $repo"
-    cd $repo
+    echo "Publishing source package ${packageModel[packageName]}"
+    cd $BUILD_DIR/${packageModel[packageName]}/${packageModel[buildPath]}
     version=`dpkg-parsechangelog --show-field Version`
-    cd ..
+    cd $BUILD_DIR
 
-    dput -f $PPA_URL $repo\_$version\_source.changes
+    echo dput -f $PPA_URL ${packageModel[packageName]}\_$version\_source.changes
 }
 
 # Main
@@ -85,18 +72,20 @@ if [ ! -d $BUILD_DIR ]; then
     mkdir -p $BUILD_DIR
 fi
 
-cd $BUILD_DIR
 echo "Generating Regolith packages in $BUILD_DIR"
 
-for package_desc in "${repos[@]}"
-do
-    repo=`echo $package_desc | cut -d'|' -f1`
-    src_url=`echo $package_desc | cut -d'|' -f2`
-    pkg_branch=`echo $package_desc | cut -d'|' -f3`
-    echo "Building $repo on branch $pkg_branch from a$src_url\a"
+typeset -A packageModel
+cd $BUILD_DIR
+
+cat $PACKAGE_MODEL_FILE | \
+jq -rc '.packages[]' | while IFS='' read package; do
+    while IFS== read -r key value; do
+        packageModel["$key"]="$value"
+    done < <( echo $package | jq -r 'to_entries | .[] | .key + "=" + .value')
 
     checkout 
     package 
     build 
     publish 
 done
+
