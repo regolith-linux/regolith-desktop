f561831fc1f8415f4322e816638bac18b2237c61
Ken Gilmer <kgilmer@gmail.com>
2019-09-27 21:31:17 -0700

    Build all packages in Regolith.  Testing in progress.

--- build.sh
+++ build.sh
@@ -5,66 +5,65 @@
 set -e
 
 # Input Parameters
-PACKAGE_MODEL_FILE=$( realpath $1 )
-PPA_URL=$2
-BUILD_DIR=$3
-
-if [ "$PACKAGE_MODEL_FILE" == "" ]; then
+if [ "$#" -ne 3 ]; then
     echo "Usage: build.sh <package model> <user PPA> <build dir>"
     echo "Must specify a package model file."
     exit 1
 fi
 
-if [ "$PPA_URL" == "" ]; then
-    echo "Usage: build.sh <package model> <user PPA> <build dir>"
-    echo "Must specify a PPA."
-    exit 1
-fi
-
-if [ "$BUILD_DIR" == "" ]; then
-    echo "Usage: build.sh <package model> <user PPA> <build dir>"
-    echo "Must specify a build dir."
-    exit 1
-fi
+PACKAGE_MODEL_FILE=$( realpath $1 )
+PPA_URL=$2
+BUILD_DIR=$3
 
 # Checkout
 function checkout {
-    echo "Checking out ${packageModel[gitRepoUrl]}"
+    repo_url=${packageModel[gitRepoUrl]}
+    repo_path=${repo_url##*/}
+    repo_name=${repo_path%%.*}
+    if [ -d $BUILD_DIR/$repo_name ]; then
+        echo "Skipping checkout, $repo_name already exists."
+        return 0
+    fi
 
-    git clone ${packageModel[gitRepoUrl]} -b ${packageModel[packageBranch]} $BUILD_DIR/${packageModel[packageName]}
+    figlet "Checking out ${packageModel[gitRepoUrl]}"
+
+    cd $BUILD_DIR
+    git clone ${packageModel[gitRepoUrl]} -b ${packageModel[packageBranch]}
+    cd -
 }
 
 # Package 
 function package {
-    cd $BUILD_DIR/${packageModel[packageName]}/${packageModel[buildPath]}
+    figlet "Preparing source for ${packageModel[packageName]}"
+    cd $BUILD_DIR/${packageModel[buildPath]}
     debian_version=`dpkg-parsechangelog --show-field Version | cut -d'-' -f1`
     cd $BUILD_DIR
 
     if [ "${packageModel[upstreamTarball]}" != "" ]; then
         echo "Downloading source from ${packageModel[upstreamTarball]}..."
-        wget ${packageModel[upstreamTarball]} -O ${packageModel[packageName]}\_$debian_version.orig.tar.gz
+        wget ${packageModel[upstreamTarball]} -O ${packageModel[buildPath]}/../${packageModel[packageName]}\_$debian_version.orig.tar.gz
     else
         echo "Generating source tarball from git repo."
-        tar cfzv ${packageModel[packageName]}\_$debian_version.orig.tar.gz --exclude .git\* --exclude debian ${packageModel[packageName]}
+        tar cfzv ${packageModel[packageName]}\_$debian_version.orig.tar.gz --exclude .git\* --exclude debian ${packageModel[buildPath]}/../${packageModel[packageName]}
     fi
 }
 
 # Build
 function build {
-    echo "Building ${packageModel[packageName]}"
-    cd $BUILD_DIR/${packageModel[packageName]}/${packageModel[buildPath]}
+    figlet "Building ${packageModel[packageName]}"
+    cd $BUILD_DIR/${packageModel[buildPath]}
     debuild -S -sa
     cd $BUILD_DIR
 }
 
 # Publish
 function publish {
-    echo "Publishing source package ${packageModel[packageName]}"
-    cd $BUILD_DIR/${packageModel[packageName]}/${packageModel[buildPath]}
+    figlet "Publishing source package ${packageModel[packageName]}"
+    cd $BUILD_DIR/${packageModel[buildPath]}
     version=`dpkg-parsechangelog --show-field Version`
     cd $BUILD_DIR
 
-    echo dput -f $PPA_URL ${packageModel[packageName]}\_$version\_source.changes
+    dput -f $PPA_URL ${packageModel[buildPath]}/../${packageModel[packageName]}\_$version\_source.changes
 }
 
 # Main
